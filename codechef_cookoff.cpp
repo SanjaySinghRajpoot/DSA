668b6d624bffb802e28de63b156b5c2c28345b54
sanjay_singh_rajpoot <iamsanjayrajpoot@gmail.com>
2021-08-16 19:31:21 +0530

    new code

--- codechef_cookoff.cpp
+++ codechef_cookoff.cpp
@@ -3,16 +3,45 @@
 using namespace std;
 
 int main(){
-  
+
   int t;
   cin>>t;
   
   int n,p,k;
- 
-  while(t--){
-   
-     cin>>n>>p>>k;
-
-  }
   
+  
+  while(t--){
+    vector<int> v;
+
+    cin>>n>>p>>k;
+
+    for(int i=0; i<n; i++){
+        
+        if(i == p){
+
+        }else{
+          v.push_back(i%k);
+        }
+    }
+
+    sort(v.begin(), v.end());
+    
+    int c = p%k;
+
+    for(int i=0; i<v.size(); i++){
+          
+          if(v[i] <= c && v[i+1] >= c){
+
+               if(i%2 == 0){
+
+                 cout<<i+2<<" -- "<<endl; 
+                 break;
+               }
+               else{
+                 cout<<i+3<<" -- "<<endl;
+                 break; 
+               }
+          }
+    }
+  }
 }
